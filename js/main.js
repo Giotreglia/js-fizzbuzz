8f9b1f256ffabd7e27c27473efd6e458b18e5a5b
Giotreglia <giovanni.treglia@gmail.com>
2023-02-24 13:26:44 +0100

    Aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+/*
+    Programma per mostrare numeri da 1 a 100 in cui 
+        se i è divisibile per 3 mostra "Fizz" 
+        se i è divisibile per 5 mostra "Buzz" 
+        se i è divisibile per entrambi mostra "FizzBuzz"
+*/
+
 for (let i = 1; i <= 100; i++) {
     if (i % (3 * 5) == 0) {
         console.log("FizzBuzz");
